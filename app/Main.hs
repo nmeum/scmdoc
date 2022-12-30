a93b05a1d0cff6e26d23d9de0ae01dc83ed5069d
Sören Tempel <soeren+git@soeren-tempel.net>
2022-12-31 00:50:15 +0100

    app: Invoke the scheme parser
    Useful for testing larger inputs.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,24 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import System.Exit
+import System.IO
+import System.Environment
+
+import Parser.Scheme
+import Text.Parsec.String
+
+parse :: Parser a -> String -> IO a
+parse p fileName = parseFromFile p fileName >>= either report return
+  where
+    report err = do
+        hPutStrLn stderr $ "Error: " ++ show err
+        exitFailure
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+    args <- getArgs
+    if length args /= 1
+        then error "invalid amount of args"
+        else do
+            r <- parse scheme (head args)
+            putStrLn $ show r
