2aa18f9fd321ff42b36aefe74dcc0db0232ba73e
Sören Tempel <soeren+git@soeren-tempel.net>
2023-01-01 04:42:27 +0100

    Library: Use either instead of Maybe for error handling

--- src/SchemeDoc/Scheme/Library.hs
+++ src/SchemeDoc/Scheme/Library.hs
@@ -9,15 +9,14 @@ newtype LibraryName = MkLibName [String]
 instance Show LibraryName where
     show (MkLibName lst) = intercalate " " lst
 
-mkLibName :: Sexp -> Maybe LibraryName
+mkLibName :: Sexp -> Either String LibraryName
 mkLibName (List exprs) = MkLibName <$>
     foldr (\x acc -> case x of
         Id  ident -> fmap ((:) ident) acc
         -- TODO: Only allow exact non-negative integers.
         Number n  -> fmap ((:) (show n :: String)) acc
-        _         -> Nothing) (Just []) exprs
-mkLibName _ = Nothing
--- TODO: Actual error messages for invalid names (i.e. use Either).
+        _         -> Left "expected identifier or uinteger") (Right []) exprs
+mkLibName _ = Left "expected non-empty list"
 
 -- An R⁷RS Scheme library as defined in Section 5.6 of the standard.
 data Library = MkLibrary { name :: LibraryName
@@ -27,14 +26,14 @@ data Library = MkLibrary { name :: LibraryName
     deriving (Show)
 
 -- Check if the given s-expression constitutes an R⁷RS library declaration.
-findLibrary' :: Sexp -> Maybe Library
+findLibrary' :: Sexp -> Either String Library
 findLibrary' (List ((Id "define-library"):x:xs)) =
     fmap (\n -> MkLibrary n xs) $ mkLibName x
-findLibrary' _ = Nothing
+findLibrary' _ = Left "found no library definition"
 
 -- Find library declarations in a Scheme source.
 -- XXX: This function only considers top-level declarations.
-findLibrary :: [Sexp] -> [Library]
-findLibrary = foldl (\acc x -> case findLibrary' x of
-                        Just lib -> lib : acc
-                        Nothing  -> acc) []
+findLibrary :: [Sexp] -> Either String [Library]
+findLibrary = foldr (\x acc -> case findLibrary' x of
+                        Right lib -> fmap ((:) lib) acc
+                        Left err  -> Left err) (Right [])
