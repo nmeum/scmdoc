494c40b30320fe66529fe8e17c5da84b8f225e74
Sören Tempel <soeren+git@soeren-tempel.net>
2023-01-01 17:19:55 +0100

    Start working on high-level parser

--- src/SchemeDoc.hs
+++ src/SchemeDoc.hs
@@ -1,5 +1,8 @@
-module SchemeDoc where
+module SchemeDoc
+    (Sexp (..), Library, findLibrary)
+where
 
+-- Algebraic data type representing Scheme S-Expressions.
 data Sexp = Str      String  -- "foo"
           | Id       String  -- foo
           | Symbol   String  -- 'foo
@@ -9,3 +12,26 @@ data Sexp = Str      String  -- "foo"
           | Number   Integer
           | Comment  String
     deriving (Show, Eq)
+
+-- An R⁷RS Scheme library as defined in Section 5.6 of the standard.
+data Library = MkLibrary { name :: String
+                         --, imports :: [String]
+                         --, exports :: [String]
+                         , body :: [Sexp] }
+    deriving (Show)
+
+-- Check if the given s-expression constitutes an R⁷RS library declaration.
+findLibrary' :: Sexp -> Maybe Library
+findLibrary' (List ((Id "define-library"):(Id lname):xs)) =
+    Just $ MkLibrary lname xs
+findLibrary' _ = Nothing
+
+-- Find a library declaration in a Scheme source.
+-- This function only considers top-level declarations.
+findLibrary :: [Sexp] -> Maybe Library
+findLibrary (expr:xs) =
+    case findLibrary' expr of
+        Just lib -> Just lib
+        Nothing  -> findLibrary xs
+findLibrary [] = Nothing
+-- TODO: A source file may have multiple library declarations.
