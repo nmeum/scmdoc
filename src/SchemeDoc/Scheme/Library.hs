517badce563e454b06214eaa87c7fe60353682a7
Sören Tempel <soeren+git@soeren-tempel.net>
2023-01-01 05:00:34 +0100

    Start working on export handler

--- src/SchemeDoc/Scheme/Library.hs
+++ src/SchemeDoc/Scheme/Library.hs
@@ -1,9 +1,12 @@
 module SchemeDoc.Scheme.Library
-    (Library, findLibrary)
+    (Library, findLibrary, mkExport, ExportSpec)
 where
 
 import Data.List (intercalate)
 import SchemeDoc
+
+-- TODO: Custem error type for all Eithers
+-- which also captures the failed S-Expression.
 
 newtype LibraryName = MkLibName [String]
 instance Show LibraryName where
@@ -18,9 +21,37 @@ mkLibName (List exprs) = MkLibName <$>
         _         -> Left "expected identifier or uinteger") (Right []) exprs
 mkLibName _ = Left "expected non-empty list"
 
+------------------------------------------------------------------------
+
+-- Export specification with internal name and external name.
+-- For exports which are not renamed both are the same.
+data ExportSpec = MkExport { internal :: String, external :: String }
+    deriving (Show)
+
+-- Export declaration for a library declaration.
+--
+--  <export spec> → <identifier>
+--      | (rename <identifier> <identifier>)
+--
+exportDecl :: Sexp -> Either String ExportSpec
+exportDecl (List [Id "rename", Id i1, Id i2]) = Right $ MkExport i1 i2
+exportDecl (Id i) = Right $ MkExport i i
+exportDecl _ = Left "expected identifier or rename spec"
+
+-- Export expression as part of a library declaration.
+--
+--   <export expr> → (export <export spec>*)
+--
+mkExport :: Sexp -> Either String [ExportSpec]
+mkExport (List ((Id "export"):exports)) = foldr (\x acc -> case exportDecl x of
+        Right e -> fmap ((:) e) acc
+        Left e  -> Left e) (Right []) exports
+mkExport _ = Left "expected export list"
+
+------------------------------------------------------------------------
+
 -- An R⁷RS Scheme library as defined in Section 5.6 of the standard.
 data Library = MkLibrary { name :: LibraryName
-                         --, imports :: [String]
                          --, exports :: [String]
                          , body :: [Sexp] }
     deriving (Show)
