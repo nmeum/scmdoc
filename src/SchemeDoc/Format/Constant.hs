3bb919de63ac822fd55423c1f9a2b67e255fd706
Sören Tempel <soeren+git@soeren-tempel.net>
2023-01-04 15:25:26 +0100

    Format: Parse constant definitions

--- src/SchemeDoc/Format/Constant.hs
+++ src/SchemeDoc/Format/Constant.hs
@@ -5,9 +5,16 @@ import SchemeDoc.Format.Types
 
 data Constant = Constant { consName  :: String
                          , consValue :: Sexp }
+    deriving (Show)
 
 instance Formatable Constant where
     fmt (Constant n _) = Formatted "constant" n $ Id n
 
+-- Parses a Scheme definition.
+--
+--  <definition> → (define <identifier> <expression>)
+--
 mkConstant :: Sexp -> Maybe Constant
+mkConstant (List ((Id "define"):(Id name):expr:[])) =
+    Just $ Constant name expr
 mkConstant _ = Nothing
