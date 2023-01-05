5737a53ae9d17ae2100cdff4700c16f119243627
Sören Tempel <soeren+git@soeren-tempel.net>
2023-01-05 08:39:49 +0100

    app: Perform exception handling

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
 import System.Exit
 import System.IO
 import System.Environment
+import Control.Exception
 
 import SchemeDoc
+import SchemeDoc.Error
 import SchemeDoc.Types
 import SchemeDoc.Parser.R7RS
 import SchemeDoc.Output
@@ -19,16 +22,23 @@ findDocLibs' :: [Sexp] -> IO ([DocLib])
 findDocLibs' exprs =
     case findDocLibs exprs of
         Right libs -> pure libs
-        Left err -> do
-            hPutStrLn stderr $ "Error: " ++ show err
-            exitFailure
+        Left err -> throwIO $ ErrSyntax err
 
 main' :: String -> IO ()
-main' fileName = do
-    source <- parseFromFile scheme fileName
-    libs <- findDocLibs' source
-    _ <- mapM writeDoc libs
-    pure ()
+main' fileName =
+  catch
+   ( do
+     source <- parseFromFile scheme fileName
+     libs <- findDocLibs' source
+     _ <- mapM writeDoc libs
+     pure ()
+   )
+   ( \case
+     ErrSyntax (SyntaxError expr err) ->
+        hPutStrLn stderr $ "Syntax error on expression `" ++ (show expr) ++ "`: " ++ err
+     ErrParser err ->
+        hPutStrLn stderr $ show err
+   )
 
 main :: IO ()
 main = do
