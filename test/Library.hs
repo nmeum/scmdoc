a33c6c529acdd0985e8f59b761dc19076a3a6639
Sören Tempel <soeren+git@soeren-tempel.net>
2023-03-11 08:04:17 +0100

    test: Slightly refactor Library tests

--- test/Library.hs
+++ test/Library.hs
@@ -11,51 +11,48 @@ import SchemeDoc.Format.Library
 import SchemeDoc.Types
 import Util
 
+getLibs :: String -> [Library]
+getLibs input = map snd libraries
+  where
+    -- Scheme S-expressions for the parsed input.
+    (Right expr) = parse input
+
+    -- Documented library definitions in the input
+    (Right libraries) = findDocLibs expr
+
+getLib :: String -> Library
+getLib = head . getLibs
+
+------------------------------------------------------------------------
+
 libraryParser :: TestTree
 libraryParser =
     testGroup
         "Tests for the Library parser"
         [ testCase "Library with exports and without declarations" $ do
-            let input = ";;> my comment\n(define-library (foo) (export string-length))"
-            let (Right expr) = parse input
+            let lib = getLib ";;> my comment\n(define-library (foo) (export string-length))"
 
-            let (Right libraries) = findDocLibs expr
-            assertEqual "Amount of libraries" 1 $ length libraries
-
-            let library = snd $ head libraries
-            assertEqual "Library name" "foo" $ libName library
-            assertEqual "Exports string-length" True $ libExports library "string-length"
-            assertEqual "Doesn't export foo" False $ libExports library "foo"
+            assertEqual "Library name" "foo" $ libName lib
+            assertEqual "Exports string-length" True $ libExports lib "string-length"
+            assertEqual "Doesn't export foo" False $ libExports lib "foo"
         , testCase "Library with renamed identifier" $ do
-            let input = ";;> my comment\n(define-library (foo) (export (rename string-length strlen)))"
-            let (Right expr) = parse input
+            let lib = getLib ";;> my comment\n(define-library (foo) (export (rename string-length strlen)))"
 
-            let (Right libraries) = findDocLibs expr
-            assertEqual "Amount of libraries" 1 $ length libraries
-
-            let library = snd $ head libraries
-            assertEqual "Renamed identifier" "strlen" $ libExternal library "string-length"
-        , testCase "Multiple library definitions in a single file" $ do
-            let input = ";;> foo library\n(define-library (foo))\n;;> bar library\n(define-library (bar))"
-            let (Right expr) = parse input
-
-            let (Right libs) = findDocLibs expr
+            assertEqual "Exports renamed internal" True $ libExports lib "string-length"
+            assertEqual "Doesn't export renamed external" False $ libExports lib "strlen"
+            assertEqual "Renamed identifier" "strlen" $ libExternal lib "string-length"
+        , testCase "Multiple lib definitions in a single file" $ do
+            let libs = getLibs ";;> foo lib\n(define-library (foo))\n;;> bar library\n(define-library (bar))"
             assertEqual "Amount of libraries" 2 $ length libs
 
-            assertEqual "First library name" "foo" $ libName (snd $ libs !! 0)
-            assertEqual "Second library name" "bar" $ libName (snd $ libs !! 1)
-        , testCase "Multipart library name" $ do
-            let input = ";;> my comment\n(define-library (foo  42 bar   23 baz))"
-            let (Right expr) = parse input
-
-            let (Right libs) = findDocLibs expr
-            assertEqual "" "foo 42 bar 23 baz" $ libName (snd $ head libs)
-        , testCase "Library with library declaration" $ do
-            let input = ";;> scheme example library\n(define-library (scheme example) (begin (define x 23) (define y 42)))"
-            let (Right expr) = parse input
-
-            let (Right libs) = findDocLibs expr
-            expanded <- libExpand (snd $ head libs)
+            assertEqual "First lib name" "foo" $ libName (libs !! 0)
+            assertEqual "Second lib name" "bar" $ libName (libs !! 1)
+        , testCase "Multipart lib name" $ do
+            let lib = getLib ";;> my comment\n(define-library (foo  42 bar   23 baz))"
+            assertEqual "" "foo 42 bar 23 baz" $ libName lib
+        , testCase "Library with lib declaration" $ do
+            let lib = getLib ";;> scheme example lib\n(define-library (scheme example) (begin (define x 23) (define y 42)))"
+            expanded <- libExpand lib
 
             assertEqual
                 ""
@@ -67,11 +64,8 @@ libraryParser =
                 ]
                 expanded
         , testCase "Library with multiple declarations" $ do
-            let input = ";;> my lib\n(define-library (scheme example) (begin (define x 23)) (begin (define y 42)))"
-            let (Right expr) = parse input
-
-            let (Right libs) = findDocLibs expr
-            expanded <- libExpand (snd $ head libs)
+            let lib = getLib ";;> my lib\n(define-library (scheme example) (begin (define x 23)) (begin (define y 42)))"
+            expanded <- libExpand lib
 
             assertEqual
                 ""
@@ -80,11 +74,8 @@ libraryParser =
                 ]
                 expanded
         , testCase "Library with include" $ do
-            let input = ";;> doc comment\n(define-library (some example) (include \"test/testdata/simple-include.scm\"))"
-            let (Right expr) = parse input
-
-            let (Right libs) = findDocLibs expr
-            expanded <- libExpand (snd $ head libs)
+            let lib = getLib ";;> doc comment\n(define-library (some example) (include \"test/testdata/simple-include.scm\"))"
+            expanded <- libExpand lib
 
             assertEqual "" [List [Id "begin", List [Id "define", Id "x", Str "foo"]]] expanded
         ]
